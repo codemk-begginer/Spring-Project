7a1fcd8102e2b9e705521b533763259335d4a075
codemk-begginer <metenousteve@gmail.com>
2025-08-21 06:51:28 +0100

    Delete positions directory

--- positions/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-DELETE from costumers;
-INSERT into customers (email) values ("metenousteve@gmail.com");
-INSERT into customers (email) values ("mskgold8@gmail.com");
-INSERT into customers (email) values ("stevemetenou8@gmail.com");
-INSERT into customers (email) values ("metenoup1@gmail.com");
-INSERT into customers (email) values ("gmetenou7@gmail.com");
-INSERT into customers (email) values ("mskgold11@outlook.fr");
